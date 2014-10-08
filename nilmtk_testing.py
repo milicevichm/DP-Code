499d99635c985b9ece2afd3ec8cd81d97f36bd47
Michael Milicevich <milicevichm@gmail.com>
2014-10-08 19:49:33 -0400

    changed to reflect packaged redd data

--- nilmtk_testing.py
+++ nilmtk_testing.py
@@ -3,6 +3,6 @@ from numpy import *
 from nilmtk import *
 from nilm_metadata import *
 
-# First TO-DO: figure out how to load data from the REDD dataset into the program
+# First TO-DO: figure out how to load hdf data from the REDD dataset into the program
 
 
