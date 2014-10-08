67445143531d2839a72dd478ac23c5568e36487f
Michael Milicevich <milicevichm@gmail.com>
2014-10-07 21:55:56 -0400

    added in import of modules

--- nilmtk_testing.py
+++ nilmtk_testing.py
@@ -1,5 +1,8 @@
+# Import the classes and modules to be used for processing energy dataset
 from numpy import *
 from nilmtk import *
 from nilm_metadata import *
 
-print "hello world"
+# First TO-DO: figure out how to load data from the REDD dataset into the program
+
+
